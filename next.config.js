a375d557514a6887922226ae98a350c806622f32
Aitor Ventura <aitor.ventura00@gmail.com>
2024-02-07 11:46:47 +0000

    test

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
      *
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
-    basePath: "/",
+    basePath: "",
 
     /**
      * Disable server-based image optimization. Next.js does not support
@@ -25,4 +25,4 @@ const nextConfig = {
     },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
